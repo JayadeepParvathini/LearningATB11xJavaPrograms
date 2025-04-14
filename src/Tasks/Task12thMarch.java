c981f59c2ab442d2fb057c10b7b4ef44324ead64
LENOVO <jparvathini@gmail.com>
2025-04-15 02:47:10 +0530

    Mar12 Task completed

---
+++ src/Tasks/Task12thMarch.java
@@ -0,0 +1,11 @@
+package Tasks;
+import java.lang.Math;
+public class Task12thMarch {
+    public static void main(String[] args) {
+        int x=10, y=10, z=10;
+
+        System.out.println(Math.cbrt((Math.pow(x,2))+Math.pow(y,2)-Math.abs(z)));
+
+
+    }
+}
